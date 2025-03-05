e5bf92f9301c739cc933ef9c14f1e9d193fffc11
Yb-Paradise <rogephj@gmail.com>
2025-03-05 13:31:59 +0300

    new commit

--- HospitalApp/urls.py
+++ HospitalApp/urls.py
@@ -5,7 +5,7 @@ from HospitalApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
+    path('home/', views.index, name='home'),
     path('starter-page/', views.starter, name='starter-page'),
     path('service-details/', views.service, name='service-details'),
     path('about/', views.about, name='about'),
@@ -20,6 +20,8 @@ urlpatterns = [
     path('deletecont/<int:id>', views.deletecont),
     path('editappoint/<int:id>', views.editappoint, name= 'edit-appointment-details'),
     path('editcont/<int:id>', views.editcont, name= 'edit-contact-details'),
+    path('', views.register, name= 'register'),
+    path('login/', views.login_view, name= 'login'),
 
 
 ]
--- HospitalApp/views.py
+++ HospitalApp/views.py
@@ -105,3 +105,8 @@ def editcont(request, id):
 
 
 
+def register(request):
+    return render(request,'register.html')
+
+def login_view(request):
+    return render(request,'login.html')
